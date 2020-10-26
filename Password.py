ff3d8e38d45487cb87a577bf63726b37eff4b929
Rishab Gautam <anitadevi04729@gmail.com>
2020-10-26 14:11:52 +0530

    commit

--- Password.py
+++ Password.py
@@ -2,7 +2,7 @@ import os #Importing os module to execute windows cmds.
 
 print('\n\tThe password is : "jaishreeram"\n\tyou can also visit : https://github.com/RISHAB-CREATOR/RgCommitBot to get the password.')
 print(" " )
-input("Press ENTER key to continue.......")
+input("\n\tPress ENTER key to continue.......")
 
 os.system('cls')
 os.system('bot.exe')
