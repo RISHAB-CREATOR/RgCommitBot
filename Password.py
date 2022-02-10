1b74e2502ed84e19aaa340349dda3af4ae931649
Rishab Gautam <anitadevi04729@gmail.com>
2022-02-10 20:23:41 +0530

    commit

--- Password.py
+++ Password.py
@@ -1,4 +1,4 @@
 print('The password is : "jaishreeram" ')
 print("you can also visit : https://github.com/RISHAB-CREATOR/RgCommitBot to get the password.")
 
-input(n/"Press ENTER key to continue.......")
+input("Press ENTER key to continue.......")
