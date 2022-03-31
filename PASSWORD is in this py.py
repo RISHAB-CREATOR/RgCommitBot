b07e9170bc45e6ab2ac78a76c956ceb311a5a310
Rishab Gautam <anitadevi04729@gmail.com>
2022-03-31 23:48:06 +0530

    commit

--- PASSWORD is in this py.py
+++ PASSWORD is in this py.py
@@ -4,4 +4,4 @@ print('\n\tThe password is : "jaishreeram"\n\tyou can also visit : https://githu
 input("\n\tPress ENTER key to continue.......") #Just an userfriendly enter key here good for nothing...
 
 os.system('cls') #cls inbuilt windows cmd for clearing logs.
-os.system('bot.exe') #inbuld windows cmd for executing the bot.exe
+os.system('bot.exe') #inbult windows cmd for executing the bot.exe
