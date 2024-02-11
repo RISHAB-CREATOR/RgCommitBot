b6a98b824a6b880de6c624421faa781236b70b16
Rishab Gautam <anitadevi04729@gmail.com>
2024-02-11 15:28:40 +0530

    commit

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from random import randint
 
 os.system('color a')
-for i in range(1,600):
+for i in range(1,1000):
 
     for j in range(0, randint(1,10)):
         d = str(i) + ' day ago'
