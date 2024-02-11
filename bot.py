2f254e488580d26ffc99ba979a41a7cab3dd7c37
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-02-12 02:27:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ from random import randint
 for i in range(1,365):
 
     for j in range(0, randint(1,10)):
-        d = str(i) + 'day ago'
+        d = str(i) + ' day ago'
         with open('file.txt', 'a') as file:
             file.write(d)
         os.system('git add .')
-        os.system('git commit --date="'+ d + '" -m "commait"')
+        os.system('git commit --date="' + d + '" -m "commit"')
 
 os.system('git push -u origin main')
