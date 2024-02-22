93a923a539c16edfac71384bd648384bbf818f84
Rishab Gautam <anitadevi04729@gmail.com>
2024-02-22 17:56:55 +0530

    commit

--- PASSWORD is in this py.py
+++ PASSWORD is in this py.py
@@ -1,5 +1,7 @@
 import os #Importing os module to execute windows cmds.
+import ctypes #Importing ctypes to change the cmd prompt title.
 
+ctypes.windll.kernel32.SetConsoleTitleW("RgCommitBot") #This title is configurable.
 print('\n\tThe password is : "jaishreeram"\n\tyou can also visit : https://github.com/RISHAB-CREATOR/RgCommitBot to get the password.') #Password and links part.
 input("\n\tPress ENTER key to continue.......") #Just an userfriendly enter key here good for nothing...
 
